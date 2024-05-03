ae00a9fd7b990a59359f28a701f8924f5c01c05f
Denys Tryfonov <trifonowden123@gmail.com>
2024-05-03 17:29:20 +0300

    asd

--- f2/controllers/contactsControllers.js
+++ f2/controllers/contactsControllers.js
@@ -1,27 +1,27 @@
-import { listContacts, getContactById, removeContact, addConntact, patchContact } from "../services/contactsServices.js";
+import { listContacts, getContactById, removeContact, addConntact, patchContact } from '../services/contactsServices.js';
 
 export const getAllContacts = async (req, res) => {
-  const contacts = await listContacts()
-  res.status(200).json(contacts)
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
 };
 
 export const getOneContact = async (req, res) => {
-  const constact = await getContactById(req.params.id)
-  res.status(200).json(constact)
+    const constact = await getContactById(req.params.id);
+    res.status(200).json(constact);
 };
 
 export const deleteContact = async (req, res) => {
-  const constact = await removeContact(req.params.id)
-  res.status(200).json(constact)
+    const constact = await removeContact(req.params.id);
+    res.status(200).json(constact);
 };
 
 export const createContact = async (req, res) => {
-  const contact = await addConntact(req.body)
-  res.status(201).json(contact)
+    const contact = await addConntact(req.body);
+    res.status(201).json(contact);
 };
 
 export const updateContact = async (req, res) => {
-  console.log(Object.keys(req.body).length);
-  const contact = await patchContact(req.params.id, req.body)
-  res.status(200).json(contact)
+    console.log(Object.keys(req.body).length);
+    const contact = await patchContact(req.params.id, req.body);
+    res.status(200).json(contact);
 };
--- f2/services/contactsServices.js
+++ f2/services/contactsServices.js
@@ -1,51 +1,48 @@
-import fs from 'fs/promises'
-import path from 'path'
+import fs from 'fs/promises';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import { v4 as uuidv4 } from 'uuid'
-
+import { v4 as uuidv4 } from 'uuid';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const contactsPath = path.join(__dirname, '..', 'db', 'contacts.json')
+const contactsPath = path.join(__dirname, '..', 'db', 'contacts.json');
 // console.log(fileURLToPath(import.meta.url));
 
 export const listContacts = async () => {
-  const res = JSON.parse(await fs.readFile(contactsPath))
-  return res
-}
+    const res = JSON.parse(await fs.readFile(contactsPath));
+    return res;
+};
 
 export const getContactById = async (id) => {
-  const list = await listContacts()
-  const contact = list.find((obj) => obj.id === id)
-  return contact ? contact : null
-}
+    const list = await listContacts();
+    const contact = list.find((obj) => obj.id === id);
+    return contact ? contact : null;
+};
 
 export const removeContact = async (id) => {
-  const list = await listContacts()
-  const index = list.findIndex(obj => obj.id === id)
-  if (index === -1) {
-    return null
-  }
-  const newList = list.splice(index, 1)
-  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2))
-  return newList[0]
-}
-
+    const list = await listContacts();
+    const index = list.findIndex((obj) => obj.id === id);
+    if (index === -1) {
+        return null;
+    }
+    const newList = list.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    return newList[0];
+};
 
 export const addConntact = async (data) => {
-  console.log(data);
-  const list = await listContacts()
-  const contact = { id: uuidv4(), ...data }
-  list.push(contact)
-  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2))
-  return contact
-}
-
+    console.log(data);
+    const list = await listContacts();
+    const contact = { id: uuidv4(), ...data };
+    list.push(contact);
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    return contact;
+};
 
 export const patchContact = async (id, data) => {
-  const list = await listContacts()
-  const index = list.findIndex(obj => id === obj.id)
-  list[index] = { id, ...data }
-  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2))
-  return list[index]
-}
+    const list = await listContacts();
+    const index = list.findIndex((obj) => id === obj.id);
+    list[index] = { id, ...data };
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    return list[index];
+};
